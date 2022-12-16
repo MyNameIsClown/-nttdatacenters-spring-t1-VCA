5adc49e6d0dd5490c5dbe149717dc968c2dc08af
MyNameIsClown <96545817+MyNameIsClown@users.noreply.github.com>
2022-12-16 09:24:46 +0100

    Initial commit

---
+++ src/main/java/com/nttdata/spring/NttdatacentersSpringT1VcaApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttdatacentersSpringT1VcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttdatacentersSpringT1VcaApplication.class, args);
+	}
+
+}
